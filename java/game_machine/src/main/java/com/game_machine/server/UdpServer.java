e006df0364bed460cb88515df827100932a95b00
Chris Ochs <cochs@playdom.com>
2013-04-20 01:22:41 -0700

    updates

--- java/game_machine/src/main/java/com/game_machine/server/UdpServer.java
+++ java/game_machine/src/main/java/com/game_machine/server/UdpServer.java
@@ -37,46 +37,46 @@ public class UdpServer {
 		log.setLevel(UdpServer.logLevel);
 	}
 
-	public void start(final UdpServerHandler handler) {
-		log.warning("Server Starting");
+	private void configure() {
+		final UdpServerHandler handler = new UdpServerHandler();
 		final DefaultEventExecutorGroup executor = new DefaultEventExecutorGroup(10);
 		final ThreadFactory acceptFactory = new UtilThreadFactory("accept");
 		acceptGroup = new NioEventLoopGroup(1, acceptFactory);
 
-		// Configure the server.
-		try {
-			boot = new Bootstrap();
-			handler.setServer(this);
-			boot.channel(NioDatagramChannel.class);
-			boot.group(acceptGroup);
-			boot.option(ChannelOption.SO_BROADCAST, false);
-			//boot.option(ChannelOption.SO_RCVBUF, 65536);
-			//boot.option(ChannelOption.SO_SNDBUF, 65536);
-			boot.handler(new ChannelInitializer<NioDatagramChannel>() {
-				@Override
-				public void initChannel(final NioDatagramChannel ch) {
-					ChannelPipeline p = ch.pipeline();
-					p.addLast(executor, new LoggingHandler(LogLevel.INFO), handler);
-				}
-			});
-
-			ChannelFuture future;
-			try {
-				future = boot.bind(hostname, port);
-				allChannels.add(future.channel());
-				future.sync();
-
-				// future.channel().closeFuture().sync();
-			} catch (InterruptedException e) {
-				log.warning("SERVER ERROR");
-				e.printStackTrace();
-				stop();
+		boot = new Bootstrap();
+		handler.setServer(this);
+		boot.channel(NioDatagramChannel.class);
+		boot.group(acceptGroup);
+		boot.option(ChannelOption.SO_BROADCAST, false);
+		// boot.option(ChannelOption.SO_RCVBUF, 65536);
+		// boot.option(ChannelOption.SO_SNDBUF, 65536);
+		boot.handler(new ChannelInitializer<NioDatagramChannel>() {
+			@Override
+			public void initChannel(final NioDatagramChannel ch) {
+				ChannelPipeline p = ch.pipeline();
+				p.addLast(executor, new LoggingHandler(LogLevel.INFO), handler);
 			}
+		});
+	}
 
-		} finally {
+	private void run() {
+		ChannelFuture future;
+		try {
+			future = boot.bind(hostname, port);
+			allChannels.add(future.channel());
+			future.sync();
+			// future.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			stop();
 		}
 	}
 
+	public void start() {
+		configure();
+		run();
+	}
+	
 	public void stop() {
 		ChannelGroupFuture f = allChannels.close();
 		try {
--- java/game_machine/src/test/java/com/game_machine/test/UdtServerTest.java
+++ java/game_machine/src/test/java/com/game_machine/test/UdtServerTest.java
@@ -14,13 +14,13 @@ import com.game_machine.server.UdtServerHandler;
 public class UdtServerTest {
 
 	public static String hostname = "192.168.1.3";
-	
-	//@Test
+
+	// @Test
 	public void runServer() throws Exception {
 		UdtServer.logLevel = Level.WARNING;
 		Client.logLevel = Level.WARNING;
 		final UdtServerHandler handler = new UdtServerHandler();
-		new UdtServer(hostname,1234).start(handler);
+		new UdtServer(hostname, 1234).start(handler);
 		Client client = new Client(hostname, 1234);
 		client.run();
 
@@ -28,26 +28,34 @@ public class UdtServerTest {
 
 	@Test
 	public void runUdp() throws Exception {
+		Runnable runnable = new Runnable() {
+			public void run() {
+				System.out.println("Run");
+			}
+		};
+		Thread t = new Thread(runnable);
+		t.start();
+		t.stop();
+		t.join();
 		UdpServer.logLevel = Level.INFO;
 		UdpClient.logLevel = Level.INFO;
-		final UdpServerHandler handler = new UdpServerHandler();
-		UdpServer server = new UdpServer(hostname,1234);
-		server.start(handler);
+		UdpServer server = new UdpServer(hostname, 1234);
+		server.start();
 		UdpClient client = new UdpClient(hostname, 1234);
 		client.run();
 		server.stop();
 
 	}
-	
+
 	@Test
 	public void actor1() {
 		try {
-			//Properties systemProperties = System.getProperties();
+			// Properties systemProperties = System.getProperties();
 			// systemProperties.setProperty("akka.log-config-on-start", "on");
-			//Pi pi = new Pi();
-			//pi.calculate(4, 10000, 10000);
-			//RemoteTest.test();
-			//Router.start();
+			// Pi pi = new Pi();
+			// pi.calculate(4, 10000, 10000);
+			// RemoteTest.test();
+			// Router.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
