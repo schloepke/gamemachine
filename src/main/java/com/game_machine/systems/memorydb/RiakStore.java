60023df0e96eaa406985d129a2fb00dcefe9876a
Chris Ochs <cochs@playdom.com>
2013-04-29 23:43:42 -0700

    updates

--- src/main/java/com/game_machine/systems/memorydb/RiakStore.java
+++ src/main/java/com/game_machine/systems/memorydb/RiakStore.java
@@ -1,5 +1,6 @@
 package com.game_machine.systems.memorydb;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -31,11 +32,18 @@ public class RiakStore extends UntypedActor {
 	private Integer minWriteInterval = 1000 / maxWritesPerSecond;
 	private Long lastWrite = System.currentTimeMillis();
 	public HashMap<String, GameObject> gameObjects = new HashMap<String, GameObject>();
-	
+	public HashMap<String, Long> gameObjectUpdates = new HashMap<String, Long>();
+	public ArrayList<GameObject> gameObjectsList = new ArrayList<GameObject>();
+	public GameObject currentGameObject = null;
+
 	public RiakStore() {
 		initRiak();
-		Cancellable cancellable = this.getContext().system().scheduler()
-				.schedule(Duration.Zero(), Duration.create(minWriteInterval, TimeUnit.MILLISECONDS), this.getSelf(), "tick", this.getContext().system().dispatcher(), null);
+		Cancellable cancellable = this
+				.getContext()
+				.system()
+				.scheduler()
+				.schedule(Duration.Zero(), Duration.create(minWriteInterval, TimeUnit.MILLISECONDS), this.getSelf(), "tick",
+						this.getContext().system().dispatcher(), null);
 	}
 
 	public void initRiak() {
@@ -45,7 +53,7 @@ public class RiakStore extends UntypedActor {
 		} catch (RiakException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
 			bucket = client.createBucket(bucketName).execute();
 			bucket = client.fetchBucket(bucketName).execute();
@@ -55,22 +63,70 @@ public class RiakStore extends UntypedActor {
 			e.printStackTrace();
 		}
 	}
-	
-	public void writeGameObject(GameObject gameObject) {
+
+	public Boolean writeGameObject(GameObject gameObject) {
 		try {
 			bucket.store("key1", gameObject).execute();
 		} catch (RiakRetryFailedException | UnresolvedConflictException | ConversionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return true;
+	}
+
+	public Boolean eligibleForWrite(GameObject gameObject) {
+		Long lastUpdated = gameObjectUpdates.get(gameObject.getId());
+
+		if ((System.currentTimeMillis() - lastUpdated) < writeInterval) {
+			// Don't update a specific object more then once every writeInterval
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public Boolean busy() {
+		if ((System.currentTimeMillis() - lastWrite) < minWriteInterval) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setGameObject(GameObject gameObject) {
+		gameObjects.put(gameObject.getId(), gameObject);
+		gameObjectUpdates.put(gameObject.getId(), System.currentTimeMillis());
 	}
 
 	public void onReceive(Object message) {
 		if (message instanceof GameObject) {
-			GameObject gameObject = (GameObject) message;
+			currentGameObject = (GameObject) message;
+			Boolean writeThrough = true;
+
+			if (gameObjects.containsKey(currentGameObject.getId())) {
+				if (!busy() && eligibleForWrite(currentGameObject)) {
+					writeThrough = true;
+				} else {
+					writeThrough = false;
+				}
+			} else if (busy()) {
+				writeThrough = false;
+			}
+
+			gameObjects.put(currentGameObject.getId(), currentGameObject);
+			if (writeThrough) {
+				if (writeGameObject(currentGameObject)) {
+					gameObjectUpdates.put(currentGameObject.getId(), System.currentTimeMillis());
+				}
+			}
+
 		} else if (message instanceof String) {
 			if (message.equals("tick")) {
 				log.info("TICK");
+				currentGameObject = new GameObject();
+				if (busy()) {
+
+				}
 			}
 		} else {
 			unhandled(message);
