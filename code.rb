1e327e5147ae6dae8d91b8e7230cb132c6710d06
Chris Ochs <chris@ochsnet.com>
2013-05-07 00:32:19 -0700

    updates

--- code.rb
+++ code.rb
@@ -1,18 +1,26 @@
 require 'lib/game_machine'
 require 'socket'
 
-java_import com.game_machine.messages.MessageUtil
-java_import com::game_machine::messages::ProtobufMessages::ClientMessage
+java_import com.game_machine.MessageUtil
+java_import com::game_machine::ProtobufMessages::ClientMessage
+
+java_import com.barchart.udt.SocketUDT
+java_import com.barchart.udt.TypeUDT
+
+socket = SocketUDT.new(TypeUDT::DATAGRAM)
 
 #String.from_java_bytes(blob.to_java_bytes)
 
 threads = []
+count = 0
+tmpcount = 0
 
 1.times do
   threads << Thread.new do
 
-    port = 1234
+    port =8100 
     host = "192.168.1.3"
+    #host = "192.168.130.128"
 
     s = UDPSocket.new
     s.connect(host,port)
@@ -27,13 +35,19 @@ threads = []
           #puts 'got'
           msg = ClientMessage.parse_from(text.to_java_bytes)
           puts "got #{msg.get_body.to_string_utf8}"
+      count += 1
+      tmpcount += 1
+      if tmpcount >= 1000
+        puts count
+        tmpcount = 0
+      end
         end
       rescue IO::WaitReadable => e
         #IO.select([s],[],[],0.02)
         # retry
       end
 
-      sleep 0.40
+      sleep 0.80
     end
   end
 end
