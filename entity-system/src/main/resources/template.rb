ea95f7e154077642e255c4f80da4586617a823c4
Chris Ochs <cochs@playdom.com>
2013-05-15 23:12:37 -0700

    updates

--- entity-system/src/main/resources/template.rb
+++ entity-system/src/main/resources/template.rb
@@ -15,16 +15,31 @@ java_import java.lang.System
 java_import com.game_machine.entity_system.generated.Components
 
 class Template
-  def create_components(names)
+  
+  def create_components
+    components_fields = Components.getFields
+    src_files = components_fields.clone
+    src_files << 'components'
+    src_class_names = src_files.collect {|name| name.slice(0,1).capitalize + name.slice(1..-1)}
+    
     user_dir =  System.getProperties["user.dir"]
     template_file = File.join(user_dir,'src','main','resources','entities.erb')
-    components_file = File.join(user_dir,'src','main','java','com','game_machine','entity_system','generated','Components.java')
-    out = ERB.new(File.read(template_file)).result(binding)
-    components_src = File.read(components_file)
-    components_src.sub!(/\/\/__REPLACE_1_START__.*\/\/__REPLACE_1_END__/m,out)
-    File.open(components_file,'w') {|f| f.write components_src}
+    
+    src_class_names.each do |klass|
+      src_file = File.join(user_dir,'src','main','java','com','game_machine','entity_system','generated',"#{klass}.java")
+      if klass == 'Components'
+        out = ERB.new(File.read(template_file)).result(binding)
+      else
+        out = ''
+      end
+      
+      src = File.read(src_file)
+      src.sub!(/\/\/__REPLACE_1_START__.*\/\/__REPLACE_1_END__/m,out)
+      File.open(src_file,'w') {|f| f.write src}
+    end
+    
     true
   end
 end
 
-Template.new.create_components(Components.getFields)
+Template.new.create_components
