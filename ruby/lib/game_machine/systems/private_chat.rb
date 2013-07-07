c656eed8bfe45b5ada2b594e171f231dfe5de529
Chris Ochs <chris@ochsnet.com>
2013-07-06 19:26:17 -0700

    updates

---
+++ ruby/lib/game_machine/systems/private_chat.rb
@@ -0,0 +1,20 @@
+module GameMachine
+  module Systems
+    class PrivateChat < Actor
+
+      def preStart
+        message = Subscribe.new.set_topic('private')
+        MessageQueuel.find.tell(message,ActorRef.new(get_self))
+      end
+
+      def on_receive(message)
+        if message.is_a?(ChatMessage)
+          send_message(message)
+        elsif message.is_a?(JavaLib::DistributedPubSubMediator::SubscribeAck)
+        else
+          unhandled(message)
+        end
+      end
+    end
+  end
+end
