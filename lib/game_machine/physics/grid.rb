e449554a34345a02942e9b6ba13b6f864bfe5104
Chris Ochs <chris@ochsnet.com>
2013-07-25 22:53:16 -0700

    updates

--- lib/game_machine/physics/grid.rb
+++ lib/game_machine/physics/grid.rb
@@ -2,46 +2,99 @@ require 'matrix'
 
 module GameMachine
   module Physics
+
+    class GridPoint
+
+      attr_reader :id
+      attr_accessor :x, :y
+      def initialize(id,x,y)
+        @id = id
+        @x = x
+        @y = y
+      end
+    end
+
     class Grid
 
       def initialize(max,cell_size)
         @max = max
-        @cell_size = cell_size
-        @hash = {}
         @min = 0
-        @width = @max / @cell_size
-        @conv_factor = 1.0/@cell_size
-        @cell_count = (@width * @width)
+        @cell_size = cell_size
+        @object_index = {}
+        @cells = {}
       end
 
       def neighbors(x,y,radius)
         offset = radius/@cell_size
-        points = []
+        cells = [hash(x,y)]
 
         1.upto(offset) do |i|
           bounds = radius * i
-          points << hash(x - bounds,y - bounds)
-          points << hash(x - bounds,y + bounds)
+          cells << hash(x - bounds,y - bounds)
+          cells << hash(x - bounds,y + bounds)
 
-          points << hash(x + bounds,y - bounds)
-          points << hash(x + bounds,y + bounds)
+          cells << hash(x + bounds,y - bounds)
+          cells << hash(x + bounds,y + bounds)
 
-          points << hash(x - bounds,y)
-          points << hash(x + bounds,y)
+          cells << hash(x - bounds,y)
+          cells << hash(x + bounds,y)
 
-          points << hash(x,y - bounds)
-          points << hash(x,y + bounds)
+          cells << hash(x,y - bounds)
+          cells << hash(x,y + bounds)
         end
-        points.delete_if {|point| point < @min || point > (@cell_count - 1)}
-        points
+
+        cells.delete_if {|cell| cell < @min || cell > (cell_count - 1)}
+        cells.uniq!
+        cells.map do |cell|
+          points = points_in_cell(cell)
+          points.empty? ? nil : points
+        end.compact.flatten
+      end
+
+      def points_in_cell(cell)
+        @cells.fetch(cell,{}).values
+      end
+
+      def get(id)
+        @object_index.fetch(id,nil)
+      end
+
+      def remove(id)
+        if cell = @object_index.fetch(id,nil)
+          if @cells.has_key?(cell)
+            @cells[cell].delete(id)
+          end
+          @object_index.delete(id)
+        end
+      end
+
+      def set(id,x,y)
+        cell = hash(x,y)
+        @object_index[id] = cell
+        @cells[cell] ||= {}
+        @cells[cell][id] = GridPoint.new(id,x,y)
+      end
+
+      private
+
+      def conv_factor
+        1.0/@cell_size
+      end
+
+      def width
+        @max / @cell_size
+      end
+
+      def cell_count
+        width * width
       end
 
       def hash(x,y)
-        (x*@conv_factor).to_i + (y*@conv_factor).to_i * @width
+        (x*conv_factor).to_i + (y*conv_factor).to_i * width
       end
 
       def hash2(x,y)
-        (x/@cell_size).floor + (y/@cell_size).floor * @width
+        (x/@cell_size).floor + (y/@cell_size).floor * width
       end
 
     end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,15 +1,62 @@
 require 'spec_helper'
 require 'matrix'
 module GameMachine
-  describe "Grid" do
+  describe Physics::Grid do
 
-    it "workds" do
-      grid = Physics::Grid.new(100,25)
-      puts grid.hash(30,30)
-      puts grid.neighbors(30,30,50).inspect
-      puts Benchmark.realtime { 100_000.times { grid.neighbors(30,30,50) } }
-      #puts Benchmark.realtime { 10_000.times {grid.hash(200,3)} }
-      #puts Benchmark.realtime { 10_000.times {grid.hash2(200,3)} }
+    subject do
+      Physics::Grid.new(100,25)
+    end
+
+    describe "#set" do
+      it "creates a grid point in correct cell" do
+        subject.set(1,0,0)
+        point = subject.points_in_cell(0).first
+        expect(point).to be_a(GameMachine::Physics::GridPoint)
+        expect(point.id).to eq(1)
+      end
+    end
+
+    describe "#get" do
+      it "Returns the cell for the given id" do
+        subject.set(1,0,0)
+        expect(subject.get(1)).to eq (0)
+      end
+    end
+
+    describe "#remove" do
+      it "Removes the cell entry" do
+        subject.set(1,0,0)
+        subject.remove(1)
+        points = subject.points_in_cell(0)
+        expect(points).to eq([])
+      end
+    end
+
+    describe "#neighbors" do
+
+      it "returns empty array when no neighbors found" do
+        expect(subject.neighbors(0,0,25)).to eq([])
+      end
+
+      it "returns all objects within given radius of cell" do
+        subject.set(1,0,0)
+        points = subject.neighbors(0,0,25)
+        grid_point = points.first
+        expect(grid_point.id).to eq(1)
+      end
+
+      it "does not return cells out of range" do
+        subject.set(1,0,0)
+        expect(subject.neighbors(50,50,25)).to eq([])
+      end
+
+      it "returns multiple points in the same cell" do
+        subject.set(1,0,0)
+        subject.set(2,0,0)
+        points = subject.neighbors(0,0,25)
+        expect(points.first.id).to eq(1)
+        expect(points.last.id).to eq(2)
+      end
     end
   end
 end
