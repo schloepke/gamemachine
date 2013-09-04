eacd285a64e792645aac1897c5d176b7e9e5e446
Chris Ochs <chris@ochsnet.com>
2013-09-04 00:10:57 -0700

    demo docs

--- lib/demo/combat_controller.rb
+++ lib/demo/combat_controller.rb
@@ -1,7 +1,28 @@
 module Demo
   class CombatController < GameMachine::Actor::Base
+    include GameMachine::ObjectDbProc
+    include GameMachine
 
     aspect %w(Attack)
+
+    dbproc(:update_health) do |entity|
+      if player = entity.player
+        if player.has_health
+          player.health.health -= 10
+        end
+
+        send_to_player(player)
+      end
+      entity
+    end
+
+    def self.send_to_player(player)
+        update_entity = Entity.new
+        update_entity.set_id(player.id)
+        update_entity.set_player(player)
+        update_entity.set_send_to_player(true)
+        PlayerGateway.find.tell(update_entity)
+    end
 
     def post_init(*args)
 
@@ -12,15 +33,8 @@ module Demo
       if message.has_attack
         attacker = message.attack.attacker
         target = message.attack.target
-        #puts "attacker=#{attacker} target=#{target}"
         attacker = GameMachine::ObjectDb.get(attacker)
-        if target_entity = GameMachine::ObjectDb.get(target)
-          player = target_entity.player
-          if player.has_health
-            player.health.health -= 10
-            GameMachine::ObjectDb.put(target_entity)
-          end
-        end
+        GameMachine::ObjectDb.call_dbproc(:update_health,target,false)
       end
     end
   end
--- lib/demo/zombie_controller.rb
+++ lib/demo/zombie_controller.rb
@@ -1,11 +1,11 @@
 module Demo
   class ZombieController < GameMachine::GameSystems::SingletonController
 
+    # Start is our initializer, called at start and restart
     def start
       position.set(entity.vector3.x, entity.vector3.y, entity.vector3.z)
       @target_position = GameMachine::Vector.new
       @last_move = Time.now.to_f
-      @last_target_time = Time.now.to_f
       @last_combat_update = Time.now.to_f
       @speed = 0.8
       unless saved_entity = GameMachine::ObjectDb.get(entity.id)
@@ -15,29 +15,38 @@ module Demo
     end
 
     def update
+
+      # Get all nearby players
       players = neighbors('player')
+
+      # Pick the first one as our target
       if players.size >= 1
         grid_value = players.get(0)
         @target_id = grid_value.id
         @target_position.set(grid_value.x,grid_value.y,grid_value.z)
         @has_target = true
-        @last_target_time = Time.now.to_f
       else
         @has_target = false
       end
 
+      # If we have a target we follow and attack if within range.
+      # track updates the grid with our location. Only needs to be called
+      # if we move.
       if @has_target
         move
         track
-        @combat_delta_time = Time.now.to_f - @last_combat_update.to_f
-        if @combat_delta_time >= 2
-          attack
-          @last_combat_update = Time.now.to_f
-        end
+        attack
       end
     end
 
     def attack
+
+      # Eligible to attack every 2 seconds
+      combat_delta_time = Time.now.to_f - @last_combat_update.to_f
+      return if combat_delta_time < 2
+
+      # If target is within range, attack.  We choose the ability to use
+      # and hand it off to the combat controller.
       distance_to_target = position.distance(@target_position)
       if distance_to_target <= 3
         attack_entity = Entity.new
@@ -49,17 +58,25 @@ module Demo
         )
         CombatController.find.tell(attack_entity,get_self)
       end
+      @last_combat_update = Time.now.to_f
     end
 
     def move
       @delta_time = Time.now.to_f - @last_move.to_f
+
+      # Especially at startup, delta time can be too long adjust if
+      # necessary
       if @delta_time >= 0.100
         @delta_time = 0.100
       end
+
+      # Simple interpolation towards the target
       position.interpolate(@target_position,@speed * @delta_time)
       @last_move = Time.now.to_f
     end
 
+    # The singleton manager sends update messages to all singleton
+    # controllers at a configurable interval.  Default 100ms.
     def on_receive(message)
       update
     end
