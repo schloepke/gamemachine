9a99be29c5c518d76876497ff1e878e7c882bbb5
Chris Ochs <chris@ochsnet.com>
2013-09-05 23:24:12 -0700

    updates

---
+++ admin/lib/model_generator.rb
@@ -0,0 +1,60 @@
+module EntitySystem
+  class ModelGenerator
+
+    def initialize(message_file)
+      @message_file = message_file
+    end
+
+    def generate
+      IO.readlines(@message_file).each do |line|
+        if md = line.match(/class (.*)< ::ProtocolBuffers::Message$/)
+          klass_name = md[1].strip
+          name = "GameMachine::Messages::#{md[1]}".strip
+          klass = name.constantize
+          instance = klass.new
+
+          names = klass.fields.map do |field|
+            field.last.name.to_s
+          end
+
+          attribute_names = names.map do |name|
+            ":#{name.underscore}"
+          end
+
+          argument_names = names.map do |name|
+            ":#{name} => #{name.underscore}"
+          end
+
+          argument_names = argument_names.join(', ')
+          attribute_names = attribute_names.join(' ')
+
+          template = File.read(template_file)
+          template.sub!('KLASS',klass_name)
+          template.gsub!('ATTRIB_METHODS',attribute_names)
+          template.sub!('MESSAGE_KLASS',name)
+          template.sub!('MESSAGE_ARGS',argument_names)
+          write_model_file(klass_name,template)
+        end
+      end
+    end
+
+    def write_model_file(klass_name,content)
+      File.open(model_file(klass_name),'w') {|f| f.write(content)}
+    end
+    def template_file
+      File.join(File.dirname(__FILE__), 'model_template.txt')
+    end
+
+    def clean
+      FileList[File.join(model_dir),'*.rb'].each {|f| FileUtils.rm f}
+    end
+
+    def model_dir
+      File.join(Rails.root,'app','models','entity_system')
+    end
+
+    def model_file(name)
+      File.join(model_dir,"#{name.underscore}.rb")
+    end
+  end
+end
