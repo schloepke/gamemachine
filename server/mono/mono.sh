e2d6a6f087cd8b7086b857a1be8b80bda04d29cd
Chris Ochs <chris@ochsnet.com>
2014-06-07 18:14:53 -0700

    mono cleanup

--- server/mono/mono.sh
+++
@@ -1,3 +0,0 @@
-export PATH=/home/chris/mono_local/bin:$PATH
-export LD_LIBRARY_PATH=/home/chris/mono_local/lib:/home/chris/game_machine/server/mono:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/home/chris/mono_local/lib/pkgconfig:$PKG_CONFIG_PATH
