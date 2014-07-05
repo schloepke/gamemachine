e9778559ad43c0464013d97603ba84343d09ec38
Chris Ochs <chris@ochsnet.com>
2014-07-05 10:36:01 -0700

    fastpath testing

--- server/java/src/main/java/com/game_machine/core/UdpServerHandler.java
+++ server/java/src/main/java/com/game_machine/core/UdpServerHandler.java
@@ -73,14 +73,15 @@ public final class UdpServerHandler extends
 			throws Exception {
 		byte[] bytes = new byte[m.content().readableBytes()];
 		m.content().readBytes(bytes);
-
+		MessageGateway.messageCount.incrementAndGet();
+		return;
 		//ClientMessage clientMessage = ClientMessage.parseFrom(bytes);
 		
-		NetMessage gameMessage = new NetMessage(null, NetMessage.UDP, bytes, m
-				.sender().getHostString(), m.sender().getPort(), ctx);
-		log.debug("MessageReceived length" + bytes.length + " "
-				+ new String(bytes));
-		this.inbound.tell(gameMessage, null);
+		//NetMessage gameMessage = new NetMessage(null, NetMessage.UDP, bytes, m
+		//		.sender().getHostString(), m.sender().getPort(), ctx);
+		//log.debug("MessageReceived length" + bytes.length + " "
+		//		+ new String(bytes));
+		//this.inbound.tell(gameMessage, null);
 
 	}
 
