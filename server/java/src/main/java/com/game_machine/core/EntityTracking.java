b031bfff03f3c42490750b93aeb66b84845c5507
Chris Ochs <chris@ochsnet.com>
2014-07-05 11:58:49 -0700

    fastpath testing

--- server/java/src/main/java/com/game_machine/core/EntityTracking.java
+++ server/java/src/main/java/com/game_machine/core/EntityTracking.java
@@ -63,7 +63,7 @@ public class EntityTracking extends UntypedActor {
 	
 	private void testValues() {
 		
-		for(Integer i=1; i<50; i++){
+		for(Integer i=1; i<500; i++){
 			Float id = randomInRange(1f,5000f);
 			System.out.println(id.toString());
 			grid.set(Float.toString(id), randomInRange(1f,2000f), randomInRange(1f,2000f), randomInRange(1f,2000f), "npc");
