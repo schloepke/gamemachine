a5fc325f82f73c096ff53c9f0e910d157384acb2
Chris Ochs <chris@ochsnet.com>
2014-08-15 17:56:01 -0700

    message updates

--- server/lib/game_machine/game_systems/remote_echo.rb
+++ server/lib/game_machine/game_systems/remote_echo.rb
@@ -15,7 +15,7 @@ module GameMachine
         if message.is_a?(String) && message == 'test'
           return
         end
-        GameMachine.logger.info "#{self.class.name} #{message}"
+        #GameMachine.logger.info "#{self.class.name} #{message}"
         commands.player.send_message(message,message.player.id)
       end
     end
