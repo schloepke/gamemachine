dc5b0ca495d2bda4e9f760489d74c01f7a9511b3
Chris Ochs <chris@ochsnet.com>
2014-11-19 17:28:12 -0800

    stats fix for windows

--- server/java/agent/bin/agent.bat
+++ server/java/agent/bin/agent.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set PLAYER_ID=controller4
+set CLOUD_HOST=cloud-dev.gamemachine.io
+set API_KEY=1a77d75e32767f89b0710311be1fd8a5
 set AKKA_HOME=%~dp0..
 set JAVA_OPTS=-Xmx40M
 set AKKA_CLASSPATH=%AKKA_HOME%\deploy\*;%AKKA_HOME%\config;%AKKA_HOME%\lib\akka\*
