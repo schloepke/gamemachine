858897f4cf7cbb2fdc88b7f4f7a69e4883226713
Chris Ochs <chris@ochsnet.com>
2014-11-19 17:29:30 -0800

    stats fix for windows

--- server/java/agent/bin/agent.bat
+++ server/java/agent/bin/agent.bat
@@ -7,4 +7,4 @@ set AKKA_HOME=%~dp0..
 set JAVA_OPTS=-Xmx40M
 set AKKA_CLASSPATH=%AKKA_HOME%\deploy\*;%AKKA_HOME%\config;%AKKA_HOME%\lib\akka\*
 
-java %JAVA_OPTS% -cp "%AKKA_CLASSPATH%" -Dakka.home="%AKKA_HOME%" akka.kernel.Main %*
+java %JAVA_OPTS% -cp "%AKKA_CLASSPATH%" -Dakka.home="%AKKA_HOME%" akka.kernel.Main io.gamemachine.client.agent.Runner
