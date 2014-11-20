48efe43c44c146b6f8adff94eeae73d1c963d595
Chris Ochs <chris@ochsnet.com>
2014-11-20 11:31:13 -0800

    fix windows batch file

--- server/bin/game_machine.bat
+++ server/bin/game_machine.bat
@@ -3,4 +3,12 @@
 set GM_HOME=%~dp0..
 set GM_CLASSPATH=%GM_HOME%\java\server\lib\*
 
-jruby -J-cp "%GM_CLASSPATH%" bin/game_machine %*
+set MAX_HEAP=500m
+set NEW_SIZE=300m
+
+set JAVA_OPTS=-Xmx%MAX_HEAP% -Xmn%NEW_SIZE% -XX:NewSize=%NEW_SIZE% -XX:MaxNewSize=%NEW_SIZE%^
+ -XX:-UseAdaptiveSizePolicy -XX:+UseParNewGC  -XX:+UseCompressedOops^
+ -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=32768 -XX:MaxTenuringThreshold=1^
+ -XX:SurvivorRatio=190 -XX:TargetSurvivorRatio=90
+
+jruby -J-cp "%GM_CLASSPATH%" bin\game_machine %*
