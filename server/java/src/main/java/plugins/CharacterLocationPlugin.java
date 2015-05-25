8cbb7d7e488e2c06b6b1aa415fe4699da9acc14b
Chris Ochs <chris@ochsnet.com>
2015-05-24 23:18:21 -0700

    character location fix

--- server/java/src/main/java/plugins/CharacterLocationPlugin.java
+++ server/java/src/main/java/plugins/CharacterLocationPlugin.java
@@ -1,10 +1,11 @@
 package plugins;
 
 import io.gamemachine.core.ActorUtil;
+import io.gamemachine.core.Plugin;
 import io.gamemachine.routing.GameMessageRoute;
 import plugins.misc.CharacterLocationHandler;
 
-public class CharacterLocationPlugin {
+public class CharacterLocationPlugin extends Plugin {
 
 	public void start() {
 		GameMessageRoute.add(CharacterLocationHandler.name,CharacterLocationHandler.name,false);
