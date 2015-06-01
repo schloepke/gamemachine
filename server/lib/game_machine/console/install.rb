593face00d20954d71da2c25c698c8b3fb9b2b11
Chris Ochs <chris@ochsnet.com>
2015-05-31 23:33:45 -0700

    plugin generator

--- server/lib/game_machine/console/install.rb
+++ server/lib/game_machine/console/install.rb
@@ -1,125 +1,75 @@
+require 'fileutils'
+
 module GameMachine
   module Console
     class Install
 
       attr_reader :install_path, :help, :install_source_path, :command
-      def initialize(command,argv,install_source_path)
-        @command = command
-        @install_path = argv.shift
-        @install_source_path = install_source_path
-        
-        puts "Installing from #{install_source_path}"
-      end
-
-      def error(reason='')
-        puts "Invalid install path #{install_path} (#{reason})"
-      end
-
-      def run!
-        if install_path.nil? || install_path.empty?
-          error("Invalid input")
-          return
-        end
-
-        if command == 'install' && File.exists?(install_path)
-          error("Directory exists")
-          return
-        end
-
-        if command == 'upgrade' && !File.exists?(install_path)
-          error("Installation not found")
-          return
-        end
-
-        @install_path = File.expand_path(install_path)
-        send(command.to_sym)
-      end
-
-      def install
-        puts "Installing into #{install_path}"
-        path = File.expand_path(install_path)
-        FileUtils.mkdir(path)
-        FileUtils.cp_r(config_source,path)
-        FileUtils.cp_r(games_source,path)
-        FileUtils.cp_r(java_source,path)
-        FileUtils.cp_r(mono_source,path)
-
-        FileUtils.cp_r(web_source,path)
-        FileUtils.cp_r(lib_source,path)
-        FileUtils.cp_r(bin_source,path)
-
-        FileUtils.cp(gemfile_source,path)
-        FileUtils.cp(gemlockfile_source,path)
-      end
-
-      def upgrade
-        puts "Upgrading installation at #{install_path}"
-        path = File.expand_path(install_path)
-
-        puts "Upgrading mono"
-        FileUtils.rm_rf(File.join(path,'mono'))
-        FileUtils.cp_r(mono_source,path)
-
-        puts 'Upgrading java'
-        FileUtils.rm_rf(File.join(path,'java','server'))
-        FileUtils.cp_r(java_project_source, File.join(path,'java','server'))
-
-        puts 'Upgrading Gemfile'
-        FileUtils.rm_rf(File.join(path,'Gemfile'))
-        FileUtils.cp(gemfile_source,path)
-
-        FileUtils.rm_rf(File.join(path,'Gemfile.lock'))
-        FileUtils.cp(gemlockfile_source,path)
-
-        puts 'Upgrading lib'
-        FileUtils.rm_rf(File.join(path,'lib'))
-        FileUtils.cp_r(lib_source,path)
-
-        puts 'Upgrading bin'
-        FileUtils.rm_rf(File.join(path,'bin'))
-        FileUtils.cp_r(bin_source,path)
+      def initialize(argv)
+        puts argv
+        @plugin = argv.shift
         
       end
 
-      def gemfile_source
-        File.join(install_source_path,'Gemfile')
+      def java_dir
+        java_dir = File.join(File.expand_path(Dir.pwd),'java','src','main','java')
       end
 
-      def gemlockfile_source
-        File.join(install_source_path,'Gemfile.lock')
+      def plugins_dir
+        File.join(java_dir,'plugins')
       end
 
-      def web_source
-        File.join(install_source_path,'web')
+      def plugin_package_dir(name)
+        File.join(plugins_dir,name)
+        FileUtils.mkdir_p(model_src)
       end
 
-      def lib_source
-        File.join(install_source_path,'lib')
+      def templates
+        File.join(java_dir,'templates')
       end
 
-      def bin_source
-        File.join(install_source_path,'bin')
+      def loader_content
+        File.read(File.join(templates,'PluginLoaderTemplate.template'))
       end
 
-      def mono_source
-        File.join(install_source_path,'mono')
+      def class_content
+        File.read(File.join(templates,'PluginTemplate.template'))
       end
 
-      def config_source
-        File.join(install_source_path,'config')
+      def loader_out
+        File.join(plugins_dir,"#{@plugin}Loader.java")
       end
 
-      def games_source
-        File.join(install_source_path,'games')
+      def plugin_dir
+        File.join(plugins_dir,@plugin.downcase)
       end
 
-      def java_project_source
-        File.join(install_source_path,'java','project')
+      def class_out
+        File.join(plugin_dir,"#{@plugin}.java")
       end
 
-      def java_source
-        File.join(install_source_path,'java')
+      def run
+        puts "Creating plugin #{@plugin}"
+        if File.exists?(loader_out) || File.exists?(class_out)
+          puts "#{@plugin} exists"
+          return
+        end
+
+        loader = loader_content
+        klass = class_content
+        loader.gsub!("CLASS",@plugin)
+        loader.gsub!("PACKAGE",@plugin.downcase)
+
+        klass.gsub!("PACKAGE",@plugin.downcase)
+        klass.gsub!("CLASS",@plugin)
+
+        FileUtils.mkdir_p(plugin_dir)
+
+        File.open(loader_out,'w') {|f| f.write loader}
+        File.open(class_out,'w') {|f| f.write klass}
+        
       end
+
 
     end
   end
