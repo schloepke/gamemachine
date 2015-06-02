102e29953af499892731f4e2d97d5224c30a3e7a
Chris Ochs <chris@ochsnet.com>
2015-06-02 09:54:01 -0700

    rework zone handling, add unity client rpc

--- server/java/src/main/java/plugins/ZoneHandlerPlugin.java
+++ server/java/src/main/java/plugins/ZoneHandlerPlugin.java
@@ -1,5 +1,9 @@
 package plugins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.gamemachine.config.AppConfig;
 import io.gamemachine.core.ActorUtil;
 import io.gamemachine.core.Plugin;
 import io.gamemachine.routing.GameMessageRoute;
@@ -7,9 +11,15 @@ import plugins.zonemanager.ZoneHandler;
 
 public class ZoneHandlerPlugin extends Plugin {
 
+	private static final Logger logger = LoggerFactory.getLogger(ZoneHandlerPlugin.class);
+	
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
+		if (!AppConfig.getOrm()) {
+			logger.warn("ZoneHandler disabled (requires an sql database and orm=true in config)");
+			return;
+		}
+		
 		GameMessageRoute.add(ZoneHandler.name,ZoneHandler.name,false);
 		ActorUtil.createActor(ZoneHandler.class,ZoneHandler.name);
 	}
