20c31b69205bbb915e4fc50806affc4b0298a6ba
Chris Ochs <chris@ochsnet.com>
2015-12-23 00:23:33 -0800

    remove java server vm only options from default startup

---
+++ server/bin/game_machine_server_vm.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set GM_HOME=%~dp0..
+set GM_CLASSPATH=%GM_HOME%\java\server\lib\*
+
+set MAX_HEAP=500m
+set NEW_SIZE=300m
+
+set JAVA_OPTS=-Xmx%MAX_HEAP% -Xmn%NEW_SIZE% -XX:NewSize=%NEW_SIZE% -XX:MaxNewSize=%NEW_SIZE%^
+ -XX:-UseAdaptiveSizePolicy -XX:+UseParNewGC  -XX:+UseCompressedOops^
+ -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=32768 -XX:MaxTenuringThreshold=1^
+ -XX:SurvivorRatio=190 -XX:TargetSurvivorRatio=90
+
+"%GM_HOME%/jruby/bin/jruby" -J-cp "%GM_CLASSPATH%" "%JAVA_OPTS%"  "%GM_HOME%/bin/game_machine" %*
+
+PAUSE
