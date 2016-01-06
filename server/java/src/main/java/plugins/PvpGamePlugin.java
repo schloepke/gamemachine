d86f9fd2b93b16e49382eb5c354e759393833cf6
Chris Ochs <chris@ochsnet.com>
2016-01-06 14:26:15 -0800

    add guild handling to core

--- server/java/src/main/java/plugins/PvpGamePlugin.java
+++ server/java/src/main/java/plugins/PvpGamePlugin.java
@@ -3,21 +3,14 @@ package plugins;
 import io.gamemachine.chat.ChatSubscriptions;
 import io.gamemachine.core.ActorUtil;
 import io.gamemachine.core.Plugin;
-import io.gamemachine.guild.GuildService;
-import io.gamemachine.routing.GameMessageRoute;
 import plugins.pvp_game.TimeHandler;
 
 public class PvpGamePlugin extends Plugin {
 
 	@Override
 	public void start() {
-			
-		
-		GameMessageRoute.add(GuildService.name,GuildService.name,false);
-		
 		
 		ActorUtil.createActor(TimeHandler.class, TimeHandler.name);
-		ActorUtil.createActor(GuildService.class, GuildService.name);
 		ActorUtil.createActor(ChatSubscriptions.class, ChatSubscriptions.name);
 
 	}
